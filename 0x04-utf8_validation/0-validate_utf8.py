200cee0d6e1c4f0835059360f86f4c4875240a28
Berekto <bereketalebachew87@gmail.com>
2023-05-18 16:19:04 +0300

    utf-8 validation fix

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,15 +1,28 @@
 #!/usr/bin/python3
-"""check if data is utf-8 valid """
-import codecs
+"""Check UTF-8 valid
+"""
 
 
 def validUTF8(data):
-    """A method that determines if
-        a given data set represents a valid UTF-8 encoding.
-    """
-    try:
-        bytes_data = bytes(data)
-        codecs.decode(bytes_data, 'utf-8')
-        return True
-    except (UnicodeDecodeError, ValueError):
-        return False
+    """a method to determine if a data is utf-8 valid """
+    remaining_bytes = 0
+
+    for byte in data:
+        if remaining_bytes == 0:
+            if byte >> 7 == 0b0:
+                continue
+            elif byte >> 5 == 0b110:
+                remaining_bytes = 1
+            elif byte >> 4 == 0b1110:
+                remaining_bytes = 2
+            elif byte >> 3 == 0b11110:
+                remaining_bytes = 3
+            else:
+                return False
+        else:
+            if byte >> 6 == 0b10:
+                remaining_bytes -= 1
+            else:
+                return False
+
+    return remaining_bytes == 0
