3c2f2f1f01db79679f801ee1b637a982f489a8fc
Berekto <bereketalebachew87@gmail.com>
2023-05-31 20:31:03 +0300

    star war characters

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const axios = require('axios');
+
+const args = process.argv.slice(2);
+
+const url = `https://swapi-api.alx-tools.com/api/films/${args[0]}`;
+async function characters (url) {
+  try {
+    const response = await axios.get(url);
+    const chars = response.data.characters;
+
+    let i = 0;
+    while (i < chars.length) {
+      const res = await axios.get(chars[i]);
+      console.log(res.data.name);
+      i++;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+characters(url);
