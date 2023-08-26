d64c7debab1170b7aabdb17a16dba883a90cd041
Berekto <bereketalebachew87@gmail.com>
2023-08-26 13:25:50 +0300

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,44 +1,57 @@
 #!/usr/bin/python3
 """ A script that reads stdin line by line and computes metrics
 """
+
 import sys
 import signal
 
+# Initialize variables to store metrics
+total_size = 0
+status_count = {}
 
-def signal_handler(signal, frame):
-    """Defines a signal handler to catch CTRL + C """
-    print_metrics()
+
+def signal_handler(sig, frame):
+    """Handler function for keyboard interruption (CTRL + C) """
+    print_statistics()
     sys.exit(0)
 
 
+def print_statistics():
+    """Printe the metrics and reset variable """
+    print(f"Total file size: {total_size}")
+    for status_code in sorted(status_count.keys()):
+        print(f"{status_code}: {status_count[status_code]}")
+    print()
+
+
 signal.signal(signal.SIGINT, signal_handler)
-total_size = 0
-status_counts = {200: 0, 301: 0, 400: 0, 401: 0,
-                 403: 0, 404: 0, 405: 0, 500: 0}
 
+try:
+    line_count = 0
+    for line in sys.stdin:
+        line = line.strip()
+        parts = line.split()
 
-def print_metrics():
-    """Define a helper function  to print the metrics"""
-    print(f"File size: {total_size}")
-    for status_code in sorted(status_counts.keys()):
-        if status_counts[status_code] > 0:
-            print(f"{status_code}: {status_counts[status_code]}")
+        if len(parts) >= 7:
+            ip, date, request, status_code, file_size = \
+                    parts[0], parts[3], parts[5], parts[6], parts[7]
 
+            if status_code.isdigit():
+                status_code = int(status_code)
+                if status_code in (200, 301, 400, 401, 403, 404, 405, 500):
+                    if status_code not in status_count:
+                        status_count[status_code] = 0
+                    status_count[status_code] += 1
 
-line_count = 0
-for line in sys.stdin:
-    try:
-        ip, _, date, request, status, size = line.split(" ")
-        method, path, protocol = request.split(" ")
-        size = int(size)
-        status = int(status)
-    except ValueError:
-        continue
+            if file_size.isdigit():
+                total_size += int(file_size)
 
-    total_size += size
-    if status in status_counts:
-        status_counts[status] += 1
+            line_count += 1
 
-    line_count += 1
-    if line_count % 10 == 0:
-        print_metrics()
+            if line_count == 10:
+                print_statistics()
+                line_count = 0
+
+except KeyboardInterrupt:
+    print("\nKeyboardInterrupt received. Printing current statistics:")
+    print_statistics()
